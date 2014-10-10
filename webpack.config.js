091ff9f3fe756725d9bdbd52fbbae0b91743dcd3
Luca Bruno <lucabru@src.gnome.org>
2014-10-11 01:48:33 +0200

    reindent

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,25 @@ var nodes = path.join(process.env.depsEnv, "lib/node_modules");
 console.log("Node modules: ", nodes);
 
 module.exports = {
-    context: __dirname,
+	context: __dirname,
 	entry: "./client/js/main.jsx",
 
 	output: {
-        path: __dirname + "/dist/",
-        filename: "bundle.js",
+		path: __dirname + "/dist/",
+		filename: "bundle.js",
 		publicPath: "/assets/"
-    },
+	},
 
 	module: {
-        loaders: [
-            { test: /\.jsx$/, loader: "jsx" },
+		loaders: [
+			{ test: /\.jsx$/, loader: "jsx" },
 			{ test: /\.scss$/, loader: "style!css!sass?outputStyle=expanded&includePaths[]=" + (path.join(nodes, "bootstrap-sass/assets/stylesheets/"))  },
 			{ test: /\.woff$/, loader: "url?limit=10000&minetype=application/font-woff" },
 			{ test: /\.ttf$/, loader: "file" },
 			{ test: /\.eot$/, loader: "file" },
 			{ test: /\.svg$/, loader: "file" },
 			{ test: /\.html$/, loader: "file?name=index.html" },
-        ],
+		],
 	},
 
 	resolve: {
